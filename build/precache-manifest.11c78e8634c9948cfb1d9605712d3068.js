aa3ed6d9dcfef77455fd913fcea5d4e81ea970a3
Ekaterina <sedpost1@mail.ru>
2020-03-04 16:14:40 +0500

    + build v

---
+++ build/precache-manifest.11c78e8634c9948cfb1d9605712d3068.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491b6d0e24e1844e731352acdab4f40c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d238d104fde83142c392",
+    "url": "/static/css/main.81814973.chunk.css"
+  },
+  {
+    "revision": "ac079c3dd22682e1c578",
+    "url": "/static/js/2.ca3395f9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.ca3395f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d238d104fde83142c392",
+    "url": "/static/js/main.0a55733e.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  },
+  {
+    "revision": "ee653919f5e86e4565eee8120929977f",
+    "url": "/static/media/loader.ee653919.svg"
+  },
+  {
+    "revision": "2981fc366400312d374c6a1f94959115",
+    "url": "/static/media/no.2981fc36.jpg"
+  }
+]);
